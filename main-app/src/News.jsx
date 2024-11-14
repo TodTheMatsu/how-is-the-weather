f996768ab8a71b0077f2ca21ba37e447191c6568
TodTheMatsu <gokudollmeanak@gmail.com>
2024-11-14 14:34:17 +0700

    News will now lead the user to the origin of the new

--- main-app/src/News.jsx
+++ main-app/src/News.jsx
@@ -1,16 +1,23 @@
 function News({ article }) {
     return (
-      <div className="w-[360px] h-[300px] bg-white  backdrop-blur-3xl bg-opacity-35  shadow-md rounded-lg flex flex-col items-center p-2 m-2">
-        <img
-          src={article.image_url || 'default-image-url.jpg'}
-          alt="News"
-          className="w-full h-[150px] object-cover rounded-t-lg"
-        />
+      <a
+        href={article.link || '#'}
+        target={article.link}       // Open in a new tab
+        rel="noopener noreferrer"  // Security measure
+        className="w-[360px] h-[300px] bg-white backdrop-blur-3xl bg-opacity-35 shadow-md rounded-lg flex flex-col items-center p-2 m-2"
+      >
+        <div className="w-full h-[150px]">
+          <img
+            src={article.image_url || 'default-image-url.jpg'}
+            alt="News"
+            className="w-full h-full object-cover rounded-t-lg"
+          />
+        </div>
         <h2 className="text-center font-bold text-sm mt-2">{article.title}</h2>
         <p className="text-xs text-center mt-1 text-gray-600">
-          {article.description?.slice(0, 50) || ''}...
+          {article.description?.slice(0, 300) || ''}...
         </p>
-      </div>
+      </a>
     );
   }
   
